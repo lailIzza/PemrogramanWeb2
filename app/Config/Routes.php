c7200d163a492c6d0bdc259aa6c694a28bac32e9
lailIzza <lailatulizzah@saintek.unipdu.ac.id>
2025-03-22 22:58:17 +0700

    praktik buat router

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// contoh router
+$routes->get('/About', 'Page::about');
+$routes->get('/Contact', 'Page::contact');
+$routes->get('/Faqs', 'Page::faqs');
+$routes->get('/Tos', 'Page/tos');
+
+// aoutoroute > untuk mematikan ubah value :false
+// $routes->setAutoRoute(true);
