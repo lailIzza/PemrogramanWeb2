3a12709935130a2428564b395d4a4b3ef4110cf9
lailIzza <lailatulizzah@saintek.unipdu.ac.id>
2025-05-27 23:41:10 +0700

    menambahkan notif ketika data terindikasi duplikat

--- app/Views/buku_view/buat.php
+++ app/Views/buku_view/buat.php
@@ -6,24 +6,38 @@
     <div class="row">
         <div class="col-8">
             <h2 class="my-3">Form Tambah data Buku</h2>
+
+            <!-- validasi -->
+            <?php if (isset($validation)) : ?>
+            <?php endif; ?>
+
             <form action="<?= base_url('/simpan'); ?>" method="post">
                 <?= csrf_field(); ?>
                 <div class="mb-3 row">
                     <label for="judul" class="col-sm-2 col-form-label">Judul</label>
                     <div class="col-sm-10">
                         <input type="judul" class="form-control <?=($validation->hasError('judul')) ? 'is-invalid': ''; ?>" id="judul" name="judul" autofocus>
+                        <div class="invalid-feedback">
+                            <?= $validation->getError('judul'); ?>
+                        </div>
                     </div>
                 </div>
                 <div class="mb-3 row">
                     <label for="penulis" class="col-sm-2 col-form-label">Penulis</label>
                     <div class="col-sm-10">
-                        <input type="penulis"  class="form-control" id="penulis" name="penulis" value="<?= old('penulis'); ?>">
+                        <input type="penulis"  class="form-control <?=($validation->hasError('penulis')) ? 'is-invalid': ''; ?>" id="penulis" name="penulis" value="<?= old('penulis'); ?>">
+                        <div class="invalid-feedback">
+                            <?= $validation->getError('penulis'); ?>
+                        </div>
                     </div>
                 </div>
                  <div class="mb-3 row">
                     <label for="penerbit" class="col-sm-2 col-form-label">Penerbit</label>
                     <div class="col-sm-10">
-                        <input type="penerbit" class="form-control" id="penerbit" name="penerbit" value="<?= old('penerbit'); ?>" >
+                        <input type="penerbit" class="form-control <?=($validation->hasError('penerbit')) ? 'is-invalid': ''; ?>" id="penerbit" name="penerbit" value="<?= old('penerbit'); ?>" >
+                        <div class="invalid-feedback">
+                            <?= $validation->getError('penerbit'); ?>
+                        </div>
                     </div>
                 </div>
                 <div class="mb-3 row">
