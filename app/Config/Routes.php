188655bff9f8451821b6c124afedcc8b5d184c2b
lailIzza <lailatulizzah@saintek.unipdu.ac.id>
2025-05-27 22:16:27 +0700

    router tgs 8

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,16 +5,25 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
 
-// contoh router
-$routes->get('/About', 'Page::about');
-$routes->get('/Contact', 'Page::contact');
-$routes->get('/Faqs', 'Page::faqs');
-$routes->get('/Tos', 'Page/tos');
+// // contoh router
+// $routes->get('/About', 'Page::about');
+// $routes->get('/Contact', 'Page::contact');
+// $routes->get('/Faqs', 'Page::faqs');
+// $routes->get('/Tos', 'Page/tos');
 
-// aoutoroute > untuk mematikan ubah value :false
-// $routes->setAutoRoute(true);
+// // aoutoroute > untuk mematikan ubah value :false
+// // $routes->setAutoRoute(true);
 
-//router untuk tugas
-$routes->get('/Biodata', 'Page::biodata');
+// //router untuk tugas
+// $routes->get('/Biodata', 'Page::biodata');
+
+//router pertemuan ke8
+$routes->get('/', 'Buku::index');
+$routes->get('/detail/(:segment)', 'Buku::detail/$1');
+$routes->get('/buat', 'Buku::buat');
+$routes->post('/simpan', 'Buku::simpan');
+$routes->delete('/hapus/(:num)', 'Buku::hapus/$1');
+$routes->get('/edit/(:segment)', 'Buku::edit/$1');
+$routes->post('/update/(:num)', 'Buku::update/$1');
