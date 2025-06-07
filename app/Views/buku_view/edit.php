ceb5061e6c8d1ddd95eda3b2c5eb711e5eff8ee1
lailIzza <lailatulizzah@saintek.unipdu.ac.id>
2025-06-07 19:32:15 +0700

    menambahkan fitur preview upload gambar

--- app/Views/buku_view/edit.php
+++ app/Views/buku_view/edit.php
@@ -5,9 +5,10 @@
     <div class="row">
         <div class="col-8">
             <h2 class="my-3">Form Ubah Data Buku</h2>
-            <form action="/update/<?= $buku['id']; ?>" method="post">
+            <form action="/update/<?= $buku['id']; ?>" method="post" enctype="multipart/form-data">
                 <?= csrf_field(); ?>
                 <input type="hidden" name="slug" value="<?= $buku['slug']; ?>">
+                <input type="hidden" name="sampulLama" value="<?= $buku['sampul']; ?>">
                 <div class="row mb-3">
                     <label for="judul" class="col-sm-2 col-form-label">Judul</label>
                     <div class="col-sm-10">
@@ -42,13 +43,19 @@
                 </div>
 
                 <div class="row mb-3">
-                    <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
-                    <div class="col-sm-10">
-                        <input type="sampul" class="form-control <?=($validation->hasError('sampul')) ? 'is-invalid' : ''; ?>" 
-                        id="sampul" name="sampul" autofocus value="<?= (old('sampul')) ? old('sampul'): $buku['sampul'] ?>">
-                        <div class="invalid-feedback">
-                            <?= $validation->getError('sampul'); ?>
+                    <label for="penerbit" class="col-sm-2 col-form-label">Sampul</label>
+                    <div class="col-sm-2">
+                        <img src="asset/gambar/no-cover.jpg" class="img-thumbnail img_preview" alt="">
+                    </div>
+
+                    <div class="col-sm-8">
+                        <div class="input-group mb-3">
+                            <input type="file" class="form-control <?= ($validation->hasError('sampul')) ? 'is-invalid' : ''; ?> " id="sampul" name="sampul" onchange="previewImage()">
+                            <div class="invalid-feedback">
+                                <?= $validation->getError('sampul'); ?>
+                            </div>
                         </div>
+                        <label for="sampul" class="input-group-text">Upload</label>
                     </div>
                 </div>
 
